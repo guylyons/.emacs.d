93094dbca7c9f2b59a2d4b7bbefdae0fbae5e13b
Guy Lyons <guylyons2@gmail.com>
2017-03-16 21:16:49 -0400

    remove unused func

--- javascript.el
+++ javascript.el
@@ -5,6 +5,8 @@
 ;; ------------------------------
 ;;
 
+(require 'js2-mode)
+
 (add-hook 'js2-mode-hook 'drag-stuff-mode)
 (add-hook 'js2-mode-hook
 	  (lambda () (flycheck-mode t)))
@@ -16,19 +18,4 @@
 (require 'flycheck)
 (add-hook 'js-mode-hook
           (lambda () (flycheck-mode t)))
-
-(require 'js2-mode)
-
-;; use local eslint from node_modules before global
-;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
-(defun my/use-eslint-from-node-modules ()
-  (let* ((root (locate-dominating-file
-                (or (buffer-file-name) default-directory)
-                "node_modules"))
-         (eslint (and root
-                      (expand-file-name "node_modules/eslint/bin/eslint.js"
-                                        root))))
-    (when (and eslint (file-executable-p eslint))
-      (setq-local flycheck-javascript-eslint-executable eslint))))
-(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
