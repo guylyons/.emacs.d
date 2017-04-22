14bfd72cdd6567f7f1211f099f3b99cd831c0477
Guy Lyons <guylyons2@gmail.com>
2017-04-22 14:34:42 -0400

    update settings

--- javascript.el
+++ javascript.el
@@ -8,14 +8,4 @@
 (require 'js2-mode)
 
 (add-hook 'js2-mode-hook 'drag-stuff-mode)
-(add-hook 'js2-mode-hook
-	  (lambda () (flycheck-mode t)))
-(add-hook 'js2-mode-hook 'company-mode)
-(add-hook 'js2-mode-hook 'skewer-mode)
 (add-hook 'js2-mode-hook 'linum-mode)
-
-
-(require 'flycheck)
-(add-hook 'js-mode-hook
-          (lambda () (flycheck-mode t)))
-
