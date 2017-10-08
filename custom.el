53496a79c7bc86922c35e4e2e5439b69ab220283
Guy Lyons <guylyons2@gmail.com>
2017-10-07 22:48:56 -0400

    move custom vars to own file

---
+++ custom.el
@@ -0,0 +1,76 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#4d4d4c" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#d6d6d6"))
+ '(cua-mode t nil (cua-base))
+ '(cursor-type (quote bar))
+ '(custom-enabled-themes (quote (rebecca)))
+ '(custom-safe-themes
+   (quote
+    ("dc9a8d70c4f94a28aafc7833f8d05667601968e6c9bf998791c39fcb3e4679c9" "7f3ef7724515515443f961ef87fee655750512473b1f5bf890e2dc7e065f240c" "67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "3a5f04a517096b08b08ef39db6d12bd55c04ed3d43b344cf8bd855bde6d3a1ae" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "125fd2180e880802ae98b85f282b17f0aa8fa6cb9fc4f33d7fb19a38c40acef0" "3b5ce826b9c9f455b7c4c8bff22c020779383a12f2f57bf2eb25139244bb7290" "9a155066ec746201156bb39f7518c1828a73d67742e11271e4f24b7b178c4710" "43c1a8090ed19ab3c0b1490ce412f78f157d69a29828aa977dae941b994b4147" "65d9573b64ec94844f95e6055fe7a82451215f551c45275ca5b78653d505bc42" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "4e4d9f6e1f5b50805478c5630be80cce40bee4e640077e1a6a7c78490765b03f" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "4486ade2acbf630e78658cd6235a5c6801090c2694469a2a2b4b0e12227a64b9" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
+ '(display-battery-mode t)
+ '(emmet-move-cursor-after-expanding t)
+ '(fci-rule-color "#37474f")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(fringe-mode 0 nil (fringe))
+ '(hl-sexp-background-color "#1c1f26")
+ '(indicate-empty-lines t)
+ '(linum-format "%d ")
+ '(neo-show-hidden-files nil)
+ '(nrepl-message-colors
+   (quote
+    ("#183691" "#969896" "#a71d5d" "#969896" "#0086b3" "#795da3" "#a71d5d" "#969896")))
+ '(package-selected-packages
+   (quote
+    (emms calfw-ical calfw-cal calfw-org zenburn-theme yari xah-lookup xah-fly-keys writeroom-mode writegood-mode web-mode web-beautify vue-mode use-package url-shortener unicode-fonts twittering-mode treemacs-projectile tramp-term thesaurus textmate tern-auto-complete term+ tabbar-ruler syntactic-close synonyms swoop swiper-helm svg sublime-themes stem ssh sqlup-mode sos solarized-theme smex smartparens smart-mode-line-powerline-theme smart-forward smart-compile slime-theme slime shell-pop scss-mode sane-term rvm ruby-refactor robe rjsx-mode rinari restclient-test replace-pairs replace+ regex-tool rebecca-theme ranger rainbow-mode rainbow-delimiters python-x python-mode pytest projectile-speedbar projectile-rails prettier-js pastelmac-theme password-vault pass paper-theme pandoc-mode osx-lib osx-dictionary org-seek org-random-todo org-password-manager org-journal org-ac oceanic-theme ob-ipython oauth2 nyan-mode nodejs-repl nlinum-hl neotree names muse multi-term move-text meacupla-theme material-theme markdown-preview-mode markdown-mode+ majapahit-theme lorem-ipsum linum-relative leuven-theme less-css-mode ledger-mode kooten-theme json-mode js-comint jedi jdee indent-tools indent-guide impatient-mode hlinum hledger-mode hl-todo hl-line+ highlight-symbol highlight-parentheses highlight-indent-guides highlight-blocks hideshow-org helm-robe helm-projectile helm-package helm-mode-manager helm-git helm-fuzzy-find helm-fuzzier helm-flyspell helm-flymake helm-flx helm-emmet helm-dictionary helm-dash helm-css-scss helm-anything helm-ag helm-ack handlebars-sgml-mode handlebars-mode hackernews gruvbox-theme grunt golden-ratio go-eldoc go gmail-message-mode github-theme github-pullrequest git-timemachine git-gutter git-gutter+ git gist fuzzy flymake-ruby figlet fancy-narrow exec-path-from-shell etable eslint-fix eshell-prompt-extras ergoemacs-mode erc-crypt encourage-mode emojify emoji-fontset emoji-cheat-sheet-plus elpy elisp-lint editorconfig dumb-jump drag-stuff dracula-theme discover-js2-refactor dired-ranger dired-k dired+ diff-hl dictionary deft dash-at-point darkburn-theme csv-mode counsel company-web company-php company-jedi company-go company-flx color-theme-sanityinc-tomorrow color-theme coffee-mode circe chess calfw cabledolphin bongo blgrep blackboard-theme bitly autopair aurora-theme atom-one-dark-theme atom-dark-theme apache-mode anzu anaphora anaconda-mode adoc-mode ac-inf-ruby)))
+ '(pdf-view-midnight-colors (quote ("#969896" . "#f8eec7")))
+ '(pos-tip-background-color "#303030")
+ '(projectile-globally-ignored-directories
+   (quote
+    (".idea" ".ensime_cache" ".eunit" ".git" ".hg" ".fslckout" "_FOSSIL_" ".bzr" "_darcs" ".tox" ".svn" ".stack-work" "dist" "public")))
+ '(save-place-mode t)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil)
+ '(uniquify-buffer-name-style nil nil (uniquify))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#f36c60")
+     (40 . "#ff9800")
+     (60 . "#fff59d")
+     (80 . "#8bc34a")
+     (100 . "#81d4fa")
+     (120 . "#4dd0e1")
+     (140 . "#b39ddb")
+     (160 . "#f36c60")
+     (180 . "#ff9800")
+     (200 . "#fff59d")
+     (220 . "#8bc34a")
+     (240 . "#81d4fa")
+     (260 . "#4dd0e1")
+     (280 . "#b39ddb")
+     (300 . "#f36c60")
+     (320 . "#ff9800")
+     (340 . "#fff59d")
+     (360 . "#8bc34a"))))
+ '(vc-annotate-very-old-color nil)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-enable-auto-expanding t)
+ '(web-mode-enable-current-element-highlight t)
+ '(xterm-color-names
+   ["#303030" "#D66F84" "#D79887" "#D49A8A" "#94B1A3" "#A8938C" "#989584" "#BAB2A9"])
+ '(xterm-color-names-bright
+   ["#3A3A3A" "#E47386" "#CC816B" "#769188" "#7D6F6A" "#9C8772" "#BAB2A9"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- go.el
+++ go.el
@@ -1,7 +1,6 @@
 ;; go.el
 ;; go lang stuffs
 
-
 (defun gofmt-before-save-hook ()
   (when eq major-mode 'go-mode)
   (message "Saving and formatting!"))
